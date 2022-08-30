69b240febe04282ad70524ff005afbb3005c015c
Bhavya Giri <bhavya.giri44@gmail.com>
2022-08-30 13:15:33 +0530

    multi problem solved

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import ChatBot from "react-simple-chatbot";
 
+
 function App() {
   return (
     <div className="App">
       <ChatBot
-        steps={[
-          {
-            id: "1",
-            message: "What is your age?",
-            trigger: "age"
-            // end: true,
-          },
-          {
-            id: "age",
-            user:true,
-            trigger:"3"
-          },
-          {
-            id:"3",
-            message:"What is your gender",
-            trigger:"gender"
-          },
-          {
-            id: 'gender',
-            options: [
-              { value: 'male', label: 'Male', end: true },
-              { value: 'female', label: 'Female', end : true},
-            ],
-          },
-        ]}
-      />
+      steps={[
+        {
+          id: '1',
+          message: 'What is your name?',
+          trigger: '2',
+        },
+        {
+          id: '2',
+          user: true,
+          trigger: '3',
+        },
+        {
+          id: '3',
+          message: 'Hi {previousValue}, nice to meet you!',
+          end: true,
+        },
+      ]}/>
     </div>
   );
 }
